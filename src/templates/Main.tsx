999ce752e8d6eeff0d5cdb018642bab7e640374c
moisestech <m@moises.tech>
2023-08-12 15:02:23 -0400

    mobile experience

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -16,51 +16,30 @@ type IMainProps = {
 };
 
 const Main = (props: IMainProps) => (
-  <div
-    className="antialiased w-full text-gray-100 px-3 md:px-0"
-    style={{ position: 'absolute', zIndex: 100 }}
-  >
+  <div className="absolute antialiased w-full text-gray-100 z-50">
     {props.meta}
 
     {/* max-w-screen-md mx-auto */}
-    <div id="home-page">
-      <div
-        className="text-scroller-wrapper"
-        style={{
-          color: 'black',
-          background: 'white',
-          fontWeight: 600,
-          position: 'fixed',
-          top: 0,
-          width: '100%',
-          zIndex: 100,
-        }}
-      >
+    <div id="homePage" className="w-full">
+      <div className="text-scroller-wrapper text-black bg-white font-bold fixed top-0 w-full z-50 bg-white-100">
         <TextScroller text="After 5,000 Years of Civilization we all need a break... ////// ARTIFICIAL INTELLIGENCE 24/7 ////// PULL THE PLUG!!! SEE FIRE, SPEAK HEAVEN!!!" />
       </div>
 
-      <section id="landing-video-wrapper" style={{ position: 'relative' }}>
+      <section id="landingVideoWrapper" className=" w-full relative">
         {/* <div className="background-video">
           <LandingVideo />
         </div> */}
 
         <div
-          className="logo-wrapper"
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            position: 'relative',
-            width: '100%',
-            height: '100%',
-            top: 0,
-            alignItems: 'center',
-          }}
+          id="logoWrapper"
+          className="flex justify-center relative w-full h-full top-0 items-center pt-20"
         >
-          <div className="logo" style={{ position: 'relative', zIndex: 100 }}>
+          <div id="logo" className="w-full relative z-50">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 1530.71 793.7"
-              style={{ height: 500, fill: 'white' }}
+              className="w-full"
+              style={{ fill: 'white' }}
             >
               <path
                 className="cls-1"
@@ -84,38 +63,17 @@ const Main = (props: IMainProps) => (
       </section>
 
       <div className="border-b border-gray-300">
-        <div
-          className="pt-16 pb-8"
-          style={{
-            display: 'flex',
-            flex: 1,
-            flexDirection: 'column',
-            justifyContent: 'center',
-          }}
-        >
-          <div
-            className="font-semibold text-6xl text-gray-900"
-            style={{ color: 'white', textAlign: 'center' }}
-          >
+        <article className="w-full items-center flex flex-1 flex-col justify-center pt-16 pb-8">
+          <span className="font-semibold text-6xl text-gray-900 text-center text-white">
             {/* {AppConfig.title} */}
             🧠 🤖 🎨
-          </div>
-          <div
-            className="text-xl"
-            style={{ color: 'white', textAlign: 'center' }}
-          >
+          </span>
+          <span className="text-xl text-white text-center md:max-w-xs lg:max-w-none">
             {AppConfig.description}
-          </div>
-        </div>
+          </span>
+        </article>
 
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            padding: 30,
-          }}
-        >
+        <div className="flex justify-center items-center p-8">
           {/* <iframe
             width="560"
             height="315"
@@ -142,12 +100,12 @@ const Main = (props: IMainProps) => (
           <Navbar>
             <li className="mr-6">
               <Link href="/">
-                <a style={{ color: 'white' }}>Home</a>
+                <a className="text-white">{'Home'}</a>
               </Link>
             </li>
             <li className="mr-6">
               <Link href="/about/">
-                <a style={{ color: 'white' }}>About</a>
+                <a className="text-white">{'About'}</a>
               </Link>
             </li>
             <li className="mr-6">
